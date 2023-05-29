0330cec9c15025c7fdee5e8b7fd1f6a672cbd251
James Corteciano <jgcorteciano@gmail.com>
2023-05-30 09:42:30 +1000

    Update(start.sh): include to delete terraform related data and logs

--- terraform/start.sh
+++ terraform/start.sh
@@ -2,9 +2,12 @@
 
 TF_LOCATION=$(dirname $0)
 
+echo "Deleting terraform files & dirs and logs..."
+rm -rf ${TF_LOCATION}/.terraform* *log
+
 date >> ${TF_LOCATION}/eks-start.date.log
 echo "===> TF initialization..." >> ${TF_LOCATION}/eks-start.date.log
-terraform -chdir=${TF_LOCATION} init
+terraform -chdir=${TF_LOCATION} init -upgrade
 echo "" >> ${TF_LOCATION}/eks-start.date.log
 
 echo "===> TF applying..." >> ${TF_LOCATION}/eks-start.date.log
